6b7043b498e836835890ff068cd9dcf5a440ad06
GMestdagh <guillaume.mestdagh@polymtl.ca>
2018-05-22 11:09:00 -0400

    All projections now return a result and a flag to indicate if the projection has been solved or not Correction of the classes linear (in)equalities projections.

--- +solvers/SpgSolver.m
+++ +solvers/SpgSolver.m
@@ -322,8 +322,8 @@ classdef SpgSolver < solvers.NlpSolver
         
         function z = project(self, x)
             %% Project - projecting x on the constraint set
-            z = self.nlp.project(x);
-            if ~self.nlp.solved
+            [z, solved] = self.nlp.project(x);
+            if ~solved
                 % Propagate throughout the program to exit
                 self.iStop = self.EXIT_PROJ_FAILURE;
             end
