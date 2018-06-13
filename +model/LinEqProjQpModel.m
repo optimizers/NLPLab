719258c21f58cd52c4b97782244d1789d71e7095
GMestdagh <mestdagh@grm.polymtl.ca>
2018-06-13 16:30:45 -0400

    Removed +model and +krylov from repository

--- +model/LinEqProjQpModel.m
+++
@@ -1,7 +0,0 @@
-classdef LinEqProjQpModel < model.QpModel & model.LinEqProj
-    methods (Access = public)
-        function self = LinEqProjQpModel(varargin)
-            self = self@model.QpModel(varargin{:});
-        end
-    end
-end
