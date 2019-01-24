5fd3f4761228d7490bcc1d27a14f7fad029f6913
GMestdagh <mestdagh@grm.polymtl.ca>
2019-01-24 13:37:51 -0500

    Added the models into the repository

---
+++ +model/PrecModel.m
@@ -0,0 +1,51 @@
+classdef PrecModel < model.NlpModel
+    %% PrecModel
+    %  A class to test preconditioning operations
+    
+    properties
+        M; % The preconditioner (H0)
+        Minv; % The inverse preconditioner (B0)
+    end
+    
+    methods
+        function o = PrecModel(name, x0, cL, cU, bL, bU, M, Minv)
+            %% Constructor
+            o = o@model.NlpModel(name, x0, cL, cU, bL, bU);
+            
+            % Add preconditioner
+            o.n = length(x0);
+            o.M = M;
+            if nargin >= 8
+                o.Minv = Minv;
+            else
+                o.Minv = [];
+            end
+        end            
+        
+        function z = precTimes(o, z)
+            z = o.M * z;
+        end
+        
+        function z = precDiv(o, z)
+            if isempty(o.Minv)
+                z = o.M \ z;
+            else
+                z = o.Minv * z;
+            end
+        end
+        
+        function zs = precSubTimes(o, zs, ind)
+            zs = o.M(ind,ind) * zs;
+        end
+        
+        function zs = precSubDiv(o, zs, ind)
+            % Warning: the result is not the same in both cases
+            if isempty(o.Minv)
+                zs = o.M(ind,ind) \ zs;
+            else
+                zs = o.Minv(ind,ind) * zs;
+            end
+        end
+    end
+end
+
